ecc8662cec8c53e857977da2bbef57953d931466
Ayush Singh <ayusjuly27@gmail.com>
2025-02-12 23:38:04 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import HomePg from "./pages/HomePg";
 import Favorites from "./pages/Favorites";
 import Navbar from "./component/Navbar";
 import { MovieProvider } from "./context/MovieContext";
+import Upcoming from "./pages/Upcoming";
+import TopRated from "./pages/TopRated";
+import TvShow from "./pages/TvShow";
 
 export default function App() {
   return (
@@ -13,6 +16,9 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePg />} />
           <Route path="/favorites" element={<Favorites />} />
+          <Route path="/upcoming" element={<Upcoming />} />
+          <Route path="/toprated" element={<TopRated />} />
+          <Route path="/tvshow" element={<TvShow />} />
         </Routes>
       </main>
     </MovieProvider>
